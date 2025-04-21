3ad3d03c710e452b939aed6eb8415ac3082133a6
aleksa0206 <aleksaobradovic569@gmail.com>
2025-04-21 11:10:39 +0200

    New Code

--- code2.py
+++ code2.py
@@ -15,3 +15,10 @@ its_coin.view()
 balance = its_coin.get_balance_of_address('Miner address')
 print('Balance of miners wallet: ', balance)
 
+print('Do again minning , for winning reward...')
+
+its_coin.mine_panding_transactions('minner_address')
+balance = its_coin.get_balance_of_address('minner_address')
+print("Balance of wallet after another minning: ", balance)
+
+its_coin.view()
